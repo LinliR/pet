a948ba55a7896816fc363d6cc7d56003dbf0c748
linrunze <1055212013@qq.com>
2024-04-27 10:58:45 +0800

    收藏功能

--- src/components/user/collection/collection.vue
+++ src/components/user/collection/collection.vue
@@ -3,7 +3,7 @@
     <el-col :span="2"></el-col>
     <el-col :span="20">
       <h1>收藏宠物</h1>
-      <el-card class="pet-content" v-for="animal in collectionList" :key="animal.id" @click="petInfo(animal)">
+      <el-card class="pet-content" v-for="animal in collectionSendList" :key="animal.id" @click="petInfo(animal)">
         <template #header>
           {{ animal.name }}
           <div v-if="animal.animalHealthInfo==null" style="display: inline-block;float: right">
@@ -22,11 +22,13 @@
         <p style="margin-top: 6px;float: right">{{ animal.province }}{{ animal.city }}</p>
       </el-card>
       <div style="margin-top: 30px;width: 100%">
-        <el-pagination style="margin-left: 40%" @current-change="loadData" v-model:current-page="current" layout="prev, pager, next" :total="total.valueOf()" />
+        <el-pagination style="margin-left: 40%" @current-change="getMyCollectionSendPet"
+                       v-model:current-page="currentSend" :pageCount="pageCountSend" layout="prev, pager, next"
+                       :total="totalSend"/>
       </div>
 
       <h1 style="margin-top: 30px">收藏寻宠</h1>
-      <el-card class="pet-content" v-for="findData in hotSearchAnimalArray" :key="findData.id"
+      <el-card class="pet-content" v-for="findData in collectionSearchList" :key="findData.id"
                @click="petInfo(findData)">
         <template #header>
           {{ findData.name }}
@@ -40,7 +42,9 @@
         <p style="margin-top: 6px;float: right">{{ findData.province }}{{ findData.city }}</p>
       </el-card>
       <div style="margin-top: 30px;width: 100%">
-        <el-pagination style="margin-left: 40%" @current-change="loadData" v-model:current-page="current" layout="prev, pager, next" :total="total.valueOf()" />
+        <el-pagination style="margin-left: 40%" @current-change="getMyCollectionSearchPet"
+                       v-model:current-page="currentSearch" :pageCount="pageCountSearch" layout="prev, pager, next"
+                       :total="totalSearch"/>
       </div>
     </el-col>
     <el-col :span="2"></el-col>
@@ -51,36 +55,53 @@
 import httpUtil from "@/http/httpUtil.js";
 
 export default {
-  data(){
-    return{
-        collectionList:[],
-      hotSearchAnimalArray:[],
-      current :1 ,
-      pageSize:10,
-      total:0,
+  data() {
+    return {
+      collectionSendList: [],
+      collectionSearchList: [],
+      currentSend: 1,
+      pageSendSize: 4,
+      totalSend: 0,
+      pageCountSend: 1,
+      currentSearch: 1,
+      pageSearchSize: 10,
+      totalSearch: 0,
+      pageCountSearch: 1,
     }
   },
-  methods:{
-    loadData:function (){
-
+  methods: {
+    getMyCollectionSendPet() {
+      let url = 'collecanimals/getCollectionList/send/' + this.currentSend + '/' + this.pageSendSize;
+      httpUtil.getData(url).then(res => {
+        console.log(res)
+        if (res.code == 200) {
+          this.collectionSendList = res.data.records;
+          this.pageCountSend = res.data.pages;
+        }
+      })
+    },
+    getMyCollectionSearchPet() {
+      let url = 'collecanimals/getCollectionList/search/' + this.currentSearch + '/' + this.pageSearchSize;
+      httpUtil.getData(url).then(res => {
+        console.log(res)
+        if (res.code == 200) {
+          this.collectionSearchList = res.data.records;
+          this.pageCountSearch = res.data.pages;
+        }
+      })
+    },
+    petInfo: function (obj) {
+      let id = obj.id
+      console.log(obj)
+      this.$router.push({
+        path: '/userPet',
+        query: {id: id}
+      })
     }
   },
   mounted() {
-    httpUtil.getData('collecanimals/getCollectionList').then(res => {
-      console.log(res)
-      if (res.code == 200) {
-        for (let i = 0; i < res.data.length; i++) {
-          this.collectionList .push(res.data[i])
-        }
-      }
-    })
-    httpUtil.getData('animalInfo/hotSearchAnimal').then(res => {
-      if (res.code == 200) {
-        for (let i = 0; i < res.data.length; i++) {
-          this.hotSearchAnimalArray.push(res.data[i])
-        }
-      }
-    })
+    this.getMyCollectionSendPet();
+    this.getMyCollectionSearchPet();
   }
 }
 </script>
